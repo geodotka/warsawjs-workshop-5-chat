847ce64cb6b5142e03b1b026ed8e5d57a8a2bdef
geodotka <dorota.kowalik@21s.pl>
2017-04-23 12:21:54 +0200

    set server config in yaml file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const config = require('./config/server.json');
+const yaml = require('js-yaml');    // https://github.com/nodeca/js-yaml
+const fs = require('fs');
+const config = yaml.safeLoad(fs.readFileSync('./config/server.yaml'));
 
-const io = require('socket.io')(config.socketPort); // wywołujemy z argumentem portu
+const io = require('socket.io')(config.serverPort); // wywołujemy z argumentem portu
 const ChatServer = require('./lib/ChatServer');
-
-// console.log(__dirname); // pokazuje aktualną ściżkę. require resolvuje ścieżki od aktualnego kataogu
 
 const server = new ChatServer({ io });
 server.init();
+
+// console.log(__dirname); // pokazuje aktualną ściżkę. require resolvuje ścieżki od aktualnego kataogu
