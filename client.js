c94570af1accdcebcf4ad38820ff0a6a0bd20e09
geodotka <dorota.kowalik@21s.pl>
2017-04-23 11:50:20 +0200

    clean prompt

--- client.js
+++ client.js
@@ -1,15 +1,23 @@
 const connection = require('socket.io-client')('http://localhost:3000');
 const readline = require('readline');
-
-connection.on('message', function({ body }){    // { body } jest wypakowanie obiektu zgodnie z ES6
-    console.log('* Server says: %s', body)
-});
-
-
 // moduł readline będzie nam zarządzał wiadomościami, czytał linie, czyścił itp.
 const rl = readline.createInterface({
     input: process.stdin, // process jest zmienną globalną w nodzie
     output: process.stdout
+});
+const util = require('util');
+const EOL = require('os').EOL;
+
+
+function writeLine(line, ...args) {
+   process.stdout.clearLine();
+   process.stdout.cursorTo(0);
+   process.stdout.write(util.format(line, ...args) + EOL);
+   rl.prompt(true);
+}
+
+connection.on('message', function({ body }){    // { body } jest wypakowanie obiektu zgodnie z ES6
+    writeLine('* Server says: %s', body);
 });
 
 rl.setPrompt('> ');
