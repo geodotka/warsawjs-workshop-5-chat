4c14036773e70a91e6d5978769cf015829fa14e0
geodotka <dorota.kowalik@21s.pl>
2017-04-23 10:50:28 +0200

    add a little ES6

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const connection = require('socket.io-client')('http://localhost:3000');
 
-connection.on('message', function(message){
-    console.log('* Server says: %s', message.body)
+connection.on('message', function({ body }){    // { body } jest wypakowanie obiektu zgodnie z ES6
+    console.log('* Server says: %s', body)
 });
