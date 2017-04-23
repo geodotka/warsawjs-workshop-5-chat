77a55a62c1d9c012493c8ea2abb1f0d0e75ab1a4
geodotka <dorota.kowalik@21s.pl>
2017-04-23 12:17:18 +0200

    set server config in json file

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const io = require('socket.io')(3000); // wywołujemy z argumentem portu
+const config = require('./config/server.json');
+
+const io = require('socket.io')(config.socketPort); // wywołujemy z argumentem portu
 const ChatServer = require('./lib/ChatServer');
 
 // console.log(__dirname); // pokazuje aktualną ściżkę. require resolvuje ścieżki od aktualnego kataogu
