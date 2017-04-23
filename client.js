88cfe406865119c4acbec7d88b9e723f8baad032
geodotka <dorota.kowalik@21s.pl>
2017-04-23 11:17:38 +0200

    send message by client

--- client.js
+++ client.js
@@ -1,5 +1,21 @@
 const connection = require('socket.io-client')('http://localhost:3000');
+const readline = require('readline');
 
 connection.on('message', function({ body }){    // { body } jest wypakowanie obiektu zgodnie z ES6
     console.log('* Server says: %s', body)
+});
+
+
+// moduł readline będzie nam zarządzał wiadomościami, czytał linie, czyścił itp.
+const rl = readline.createInterface({
+    input: process.stdin, // process jest zmienną globalną w nodzie
+    output: process.stdout
+});
+
+rl.setPrompt('> ');
+rl.prompt();
+
+rl.on('line', function(line){
+    connection.emit('message', { body: line});
+    rl.prompt();
 });
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const io = require('socket.io')(3000); // wywołujemy z argumentem portu
 
 io.on('connection', function (socket) {
-  socket.emit('message', { body: 'Hello client!' });
+    socket.on('message', function({ body }){
+        io.sockets.emit('message', { body });   // jak przyjdzie wiadomość, wyśle do wszystkich klientów
+    });
 });
